89d2d19f77690b5f944ddb8ee73ae8b9665f3a88
Moscode <odunoshomoses@gmail.com>
2021-05-20 17:36:17 +0000

    Logic of the application has been made. Two functions, one for taking the values the other for modifying the state storing the data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import '@material-ui/core/TextField'
 
 export default class App extends Component{
   constructor(props){
     super(props);
     this.state={
-            
+            userName: "Firebase",
+            todoItems: [{action: "Study maths", done: false},
+                        {action: "Study maths", done: false},
+                        {action: "Study maths", done: false},
+                        {action: "Study maths", done: false}
+           ],
+           newTodo: ""
     }
+  }
+
+//Adding the text to a property
+  updateNewTodo = (event) => {
+    this.setState({newTodo: event.target.value});
+  }
+
+//Adding the newTodo content to the todoItems
+  updateTodoItems = () => {
+    if(!this.state.todoItems.find((newText) => 
+      newText.action === this.state.newTodo
+    )){
+      this.setState({
+                    todoITems: [...this.state.todoItems, {action:this.state.newTodo, done:false}],
+                    newTodo: ""
+                  }
+        )
+    }
+  }
+
+  changeStateData = () =>{
+    this.setState({
+      userName: this.state.userName === "Firebase" ? "Google Cloud":"Firebase"
+    });
   }
 
   render(){
     return(
         <div>
-          <h1 className='bg-primary text-white text-center'>TodoAPP with Firebase</h1>
-          <TextField id="standard-basic" label="Enter Your Task" />
+          <h1 className='bg-primary text-white text-center'>
+            {`Powered By ${this.state.userName}`}
+          </h1>
+          <button className="btn btn-primary m-2"
+          onClick={ this.changeStateData }
+          >
+            Change Data
+          </button>
         </div>
     )
   }
